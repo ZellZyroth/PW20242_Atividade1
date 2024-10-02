ff53de9bba5787000c4e6e36f187100eeb053320
ZellZyroth <pedrogabrielkonteiro@gmail.com>
2024-10-01 21:51:48 -0300

    atualização geral

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
@@ -10,6 +11,21 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastro")
+def get_cadastro(request: Request):
+    return templates.TemplateResponse("cadastro.html", {"request": request})
+@app.post("/post_cadastro")
+def post_cadastro(
+    request: Request, 
+    nome: str = Form(...), 
+    descricao: str = Form(...), 
+    estoque: str = Form(...), 
+    preco: str = Form(...),
+    categoria: str = Form(...)):
+    #salvar_cadastro(nome, descricao, estoque, preco)
+    return RedirectResponse("/", 303)
+
 
 if __name__ == "__main__":
  uvicorn.run("main:app", port=8000, reload=True)
